a6550ddef4f56d09254ebdc0dafdf7f2f6d9ea88
vinicius sato <vinicius.sato@outlook.com>
2023-02-25 11:43:28 -0300

    initial

---
+++ ControleContato/Data/BancoContext.cs
@@ -0,0 +1,14 @@
+﻿using ControleContato.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace ControleContato.Data
+{
+    public class BancoContext :DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+        }
+         public DbSet<ContatoModel> Contatos{ get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# sistemaDeContatos
+Sistemas para cadastros de contatos usando Asp.NetCore6 MVC e SQLServer.
